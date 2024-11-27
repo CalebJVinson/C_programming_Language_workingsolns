79b3477b2e9ac868d48cdb3c58770229301f7289
Cal Vinson <100442011+CalebJVinson@users.noreply.github.com>
2024-11-27 13:11:06 -0600

    Update ch1p4.cpp

--- ch1p4.cpp
+++ ch1p4.cpp
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void) {
+  float ft, cel;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  printf("Celsius\tFahr\n");
+  printf("-------\t-------\n")';
+  cel = lower;
+  while (cel <= upper)
+    {
+      ft = cel * (9.0 / 5.0) + 32.0;
+      printf("%3.0f\t%6.1f\n", cel, ft);
+      cel = cel + step;
+    }
+  getchar();
+  return 0;
+}
